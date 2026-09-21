f083336d82f7d7c96794f2a2144de79ab816776b
MayankSrivasta <mayanksri94@gmail.com>
2026-09-21 09:03:59 +0530

    Add: permutation-string - submission-8

---
+++ Data Structures & Algorithms/permutation-string/submission-8.py
@@ -0,0 +1,18 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1hm = Counter(s1)
+        n = len(s1)
+
+        s2hm = Counter(s2[:n  - 1])
+        l = 0
+
+        for r in range(n - 1, len(s2)):
+            s2hm[s2[r]] += 1
+
+            if s1hm == s2hm:
+                return True
+            
+            s2hm[s2[l]] -= 1
+            l += 1
+        
+        return False
