64ea5e7fc144818aa397c5d0ef2ac83bf7d12484
MayankSrivasta <mayanksri94@gmail.com>
2026-09-21 09:45:40 +0530

    Add: permutation-string - submission-10

---
+++ Data Structures & Algorithms/permutation-string/submission-10.py
@@ -0,0 +1,20 @@
+class Solution:
+# FIXED SIZE SLIDING WINDOW
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1hm = Counter(s1)
+        n = len(s1)
+        
+        l = 0
+        s2hm = Counter(s2[:n - 1])
+
+        for r, v in enumerate(s2[n - 1: len(s2)], start = n -1):
+
+            s2hm[s2[r]] += 1
+
+            if s1hm == s2hm:
+                return True
+            
+            s2hm[s2[l]] -= 1
+            l += 1
+
+        return False
